5855793c8fe2284332cb3ea308239a58aafc8660
Isaac Joffe <isaac11joffe@gmail.com>
2023-11-06 21:02:15 -0700

    added main fragments javadocs

--- InventoryManager/app/src/main/java/com/example/inventorymanager/ui/home/HomeFragment.java
+++ InventoryManager/app/src/main/java/com/example/inventorymanager/ui/home/HomeFragment.java
@@ -25,12 +25,30 @@ import com.example.inventorymanager.databinding.FragmentHomeBinding;
 
 import java.util.ArrayList;
 
+/**
+ * Shows the list of items being tracked by the application.
+ * A brief summary of each item is displayed.
+ * Users may choose to view the details of an item, delete items, filter items, or sort items.
+ * @author Kareem Assaf, Tyler Hoekstra, Isaac Joffe, David Onchuru
+ * @see Item
+ * @see ItemViewModel
+ */
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
     private ItemAdapter adapter;
     private ArrayList<Item> items;
 
+    /**
+     * Provides the user interface of the fragment.
+     * Queries the database to obtain detailed information about each item being tracked.
+     * Displays summary information about each of these items.
+     * Allows the user to view detailed information about each item being tracked and to delete multiple items at a time.
+     * @param inflater The object used to inflate views as required.
+     * @param container The parent view of the fragment.
+     * @param savedInstanceState The previous state of the fragment; not used in this fragment.
+     * @return The root of the view.
+     */
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // basic setup functionality to set up view
         HomeViewModel homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
@@ -86,7 +104,10 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
-    // updates the total estimated value being displayed on the screen
+    /**
+     * Updates the total estimated monetary value being displayed on the screen.
+     * Computes the proper total value based on addition of each displayed item's estimated monetary value.
+     */
     public void updateTotal() {
         // calculate total estimated value
         double total = 0;
@@ -99,6 +120,9 @@ public class HomeFragment extends Fragment {
         totalTextView.setText(String.format("$%.2f", total));
     }
 
+    /**
+     * Destroys the fragment.
+     */
     @Override
     public void onDestroyView() {
         super.onDestroyView();

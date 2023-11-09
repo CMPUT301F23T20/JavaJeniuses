31c59a1e29cd8bb41f817c655ad1e581e49813b6
TylerHoekstra <89994695+TylerHoekstra@users.noreply.github.com>
2023-11-08 17:08:05 -0700

    Added some more tests, created javadocs

--- InventoryManager/app/src/androidTest/java/com/example/inventorymanager/ViewTesting.java
+++ InventoryManager/app/src/androidTest/java/com/example/inventorymanager/ViewTesting.java
@@ -13,8 +13,7 @@ import org.junit.runner.RunWith;
 @LargeTest
 /**
  * Conducts comprehensive UI testing. Tests the home view and the primary actions that can be taken
- * within the home view. Tests basic displaying, filtering, sorting, multi-selection, multi-deleting,
- * and multi-tagging*.
+ * within the home view. Tests basic filtering, sorting, multi-selection, multi-deleting, and multi-tagging*.
  * @author Tyler Hoekstra
  * @see com.example.inventorymanager.ui.home.HomeFragment
  * @see chooseFilterFragment
@@ -26,37 +25,64 @@ public class ViewTesting {
     public ActivityScenarioRule<MainActivity> scenario = new
             ActivityScenarioRule<MainActivity>(MainActivity.class);
 
-
     @Test
     /**
-     *
+     * Tests the item filtering in the home view. First login, adds multiple items, then attempt to filter
+     * them by make of the items.
      */
-    public void testItemDisplaying(){
-        // Do Login first
-        
-    }
-
-
-    @Test
-    /**
-     *
-     */
-    public void testItemFiltering(){
-        // Do Login first
-    }
-
-
-    @Test
-    /**
-     *
-     */
-    public void testItemSorting(){
+    public void testItemFilteringMake(){
         // Do Login first
     }
 
     @Test
     /**
-     *
+     * Tests the item filtering in the home view. First login, adds multiple items, then attempt to filter
+     * them by keywords in their description.
+     */
+    public void testItemFilteringKeyword(){
+        // Do Login first
+    }
+
+    @Test
+    /**
+     * Tests the item filtering in the home view. First login, adds multiple items, then attempt to filter
+     * them by date of purchase.
+     */
+    public void testItemFilteringDate(){
+        // Do Login first
+    }
+
+    @Test
+    /**
+     * Tests the item sorting in the home view. First login, adds multiple items, then attempt to sort
+     * them by make of the items.
+     */
+    public void testItemSortingMake(){
+        // Do Login first
+    }
+
+    @Test
+    /**
+     * Tests the item sorting in the home view. First login, adds multiple items, then attempt to sort
+     * them by keywords in the description of the items.
+     */
+    public void testItemSortingKeyword(){
+        // Do Login first
+    }
+
+    @Test
+    /**
+     * Tests the item sorting in the home view. First login, adds multiple items, then attempt to sort
+     * them by date of purchase of the items.
+     */
+    public void testItemSortingDate(){
+        // Do Login first
+    }
+
+    @Test
+    /**
+     * Tests selecting and deselecting of multiple items. First login, adds multiple items, selects
+     * multiple items, check for selection, deselects multiple items, checks for selection.
      */
     public void testMultiSelection(){
         // Do Login first
@@ -64,7 +90,8 @@ public class ViewTesting {
 
     @Test
     /**
-     *
+     * Tests selecting and deleting multiple items at once. First login, adds multiple items, selects
+     * multiple items, then delete and check if the items were successfully deleted.
      */
     public void testMultiDelete(){
         // Do Login first
